ce410b5a33e3c0cc81eaea5fa2bcef4c8d8bcc8b
Jorge Reyes <rjorge99@hotmail.com>
2022-06-19 17:33:19 -0500

    Se cambia terminal a modo flotante

--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -13,8 +13,8 @@ toggleterm.setup({
     start_in_insert = true,
     insert_mappings = true,
     persist_size = true,
-    -- direction = "float",
-    direction = "horizontal",
+    direction = "float",
+    -- direction = "horizontal",
     close_on_exit = true,
     shell = vim.o.shell,
     float_opts = {
