7882f725178be301825bf0a94cc7afda104abec0
Jorge Reyes <rjorge99@hotmail.com>
2022-06-27 17:01:06 -0500

    Se habilita el tab para cmp

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -81,22 +81,22 @@ cmp.setup({
             c = cmp.mapping.close(),
         }),
         ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        -- ["<Tab>"] = cmp.mapping(function(fallback)
-        --     if cmp.visible() then
-        --         cmp.select_next_item()
-        --     elseif luasnip.expandable() then
-        --         luasnip.expand()
-        --     elseif luasnip.expand_or_jumpable() then
-        --         luasnip.expand_or_jump()
-        --     elseif check_backspace() then
-        --         fallback()
-        --     else
-        --         fallback()
-        --     end
-        -- end, {
-        --     "i",
-        --     "s",
-        -- }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expandable() then
+                luasnip.expand()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            elseif check_backspace() then
+                fallback()
+            else
+                fallback()
+            end
+        end, {
+            "i",
+            "s",
+        }),
         ["<S-Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_prev_item()
